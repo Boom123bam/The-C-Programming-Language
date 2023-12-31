3c9839e25425edfc156920f2fe70b2ccf56ed6fa
Brandon <brandontang0325@gmail.com>
2023-12-31 09:23:59 +0000

    Initial commit

---
+++ Chapter 1/celcius.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define MIN 0
+#define MAX 200
+#define STEP 20
+
+float toCelcius(float fahr)
+{
+  return (fahr - 32) * 5 / 9;
+}
+
+int main()
+{
+  for (float fahr = MIN; fahr <= MAX; fahr += STEP)
+  {
+    printf("%2.f%6.1f\n", fahr, toCelcius(fahr));
+  }
+  return 0;
+}
