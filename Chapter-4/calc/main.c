8ea76f3c3ece76a6447f9fb84adee0e21f76ff0a
Brandon <brandontang0325@gmail.com>
2024-01-07 11:45:31 +0000

    add rev polish calc

---
+++ Chapter-4/calc/main.c
@@ -0,0 +1,54 @@
+/*
+ * reverse polish calculator
+ */
+
+#include "./stack.h"
+#include <stdio.h>
+
+#define MAX_INPUT 100
+
+char getNext(char input[]) {
+  static int currentIndex = 0;
+  char c;
+
+  while ((c = input[currentIndex++]) == ' ' || c == '\t') {
+  }
+
+  return c;
+}
+
+int main() {
+  char input[MAX_INPUT] = "1 2 - 3 4 + *"; // result: -7
+  char c;
+  int op2;
+
+  while ((c = getNext(input)) != '\0') {
+    if (c >= '0' && c <= '9') {
+      push(c - '0');
+      continue;
+    }
+
+    switch (c) {
+    case '+':
+      push(pop() + pop());
+      break;
+    case '*':
+      push(pop() * pop());
+      break;
+    case '-':
+      op2 = pop();
+      push(pop() - op2);
+      break;
+    case '/':
+      op2 = pop();
+      if (op2 != 0)
+        push(pop() / op2);
+      else
+        printf("division by 0 error\n");
+      break;
+    }
+  }
+
+  printf("result: %d\n", pop());
+}
+
---
+++ Chapter-4/calc/stack.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define MAX_STACK 100
+
+static int stack[MAX_STACK];
+static int stackSize = 0;
+
+void push(int num) {
+  if (stackSize < MAX_STACK - 1)
+    stack[stackSize++] = num;
+  else
+    printf("error: stack full");
+}
+
+int pop() {
+  if (stackSize > 0)
+    return stack[--stackSize];
+
+  printf("error: can't pop from empty stack");
+  return '\0';
+}
+
+void printStack() {
+  for (int i = 0; i < stackSize;) {
+    printf("%d ", stack[i++]);
+  }
+  printf("\n");
+}
