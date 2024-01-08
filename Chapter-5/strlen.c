1f130de3fb3d77bb24d9294bea63c3fbf8c39578
Brandon <brandontang0325@gmail.com>
2024-01-08 10:24:33 +0000

    add strlen.c

---
+++ Chapter-5/strlen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int strlen2(char *c);
+int strlen3(char *c);
+
+int main() {
+  char string[] = "hello world";
+
+  printf("length: %d\n", strlen3(string));
+}
+
+int strlen2(char *c) {
+  int i;
+  for (i = 0; *(c + i) != '\0'; i++) {
+  }
+  return i;
+}
+
+int strlen3(char *c) {
+  int i;
+  for (i = 0; *c != '\0'; i++, c++) {
+  }
+  return i;
+}
